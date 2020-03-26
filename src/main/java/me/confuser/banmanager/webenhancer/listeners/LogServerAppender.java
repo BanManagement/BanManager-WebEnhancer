7359c51859356e2aa3525c2b2c42ea083bfe8d55
Vasiliy <vasyokbelyivnew@gmail.com>
2020-03-27 02:34:59 +0500

    Format code, using TimeUnit

--- src/main/java/me/confuser/banmanager/webenhancer/listeners/LogServerAppender.java
+++ src/main/java/me/confuser/banmanager/webenhancer/listeners/LogServerAppender.java
@@ -1,61 +1,66 @@
 package me.confuser.banmanager.webenhancer.listeners;
 
-import com.google.common.collect.EvictingQueue;
-import lombok.Getter;
-import me.confuser.banmanager.webenhancer.WebEnhancer;
-import me.confuser.banmanager.webenhancer.data.LogData;
+import java.util.concurrent.TimeUnit;
+import java.util.regex.Pattern;
+
 import org.apache.logging.log4j.core.LogEvent;
 import org.apache.logging.log4j.core.appender.AbstractAppender;
 import org.apache.logging.log4j.core.layout.PatternLayout;
 
-import java.util.regex.Pattern;
+import com.google.common.collect.EvictingQueue;
+
+import lombok.Getter;
+import me.confuser.banmanager.webenhancer.WebEnhancer;
+import me.confuser.banmanager.webenhancer.data.LogData;
 
 public class LogServerAppender extends AbstractAppender {
 
-  private WebEnhancer plugin;
-  @Getter
-  private EvictingQueue<LogData> queue;
+    private WebEnhancer plugin;
+    @Getter
+    private EvictingQueue<LogData> queue;
 
-  public LogServerAppender(WebEnhancer plugin) {
-    super("Log4JAppender", null,
-            PatternLayout.newBuilder().withPattern("[%d{HH:mm:ss} %level]: %msg").build(), false);
+    @SuppressWarnings("deprecation")
+    public LogServerAppender(final WebEnhancer plugin) {
+        super("Log4JAppender", null, PatternLayout.newBuilder().withPattern("[%d{HH:mm:ss} %level]: %msg").build(),
+                false);
 
-    this.plugin = plugin;
-    queue = EvictingQueue.create(plugin.getConfiguration().getAmount());
-  }
-
-  @Override
-  public boolean isStarted() {
-    return true;
-  }
-
-  @Override
-  public void append(LogEvent log) {
-    String message = log.getMessage().getFormattedMessage();
-
-    if (message == null) return;
-
-    boolean ignore = false;
-
-    for (String check : plugin.getConfiguration().getContains()) {
-      if (message.contains(check)) {
-        ignore = true;
-        break;
-      }
+        this.plugin = plugin;
+        queue = EvictingQueue.create(plugin.getConfiguration().getAmount());
     }
 
-    if (!ignore) {
-      for (Pattern pattern : plugin.getConfiguration().getPatterns()) {
-        if (pattern.matcher(message).matches()) {
-          ignore = true;
-          break;
+    @Override
+    public boolean isStarted() {
+        return true;
+    }
+
+    @Override
+    public void append(final LogEvent log) {
+        String message = log.getMessage().getFormattedMessage();
+
+        if (message == null)
+            return;
+
+        boolean ignore = false;
+
+        for (String check : plugin.getConfiguration().getContains()) {
+            if (message.contains(check)) {
+                ignore = true;
+                break;
+            }
         }
-      }
-    }
 
-    if (!ignore) {
-      // Not thread safe, sync this if causes issues
-      queue.add(new LogData(message, log.getTimeMillis() / 1000L));
+        if (!ignore) {
+            for (Pattern pattern : plugin.getConfiguration().getPatterns()) {
+                if (pattern.matcher(message).matches()) {
+                    ignore = true;
+                    break;
+                }
+            }
+        }
+
+        if (!ignore) {
+            // Not thread safe, sync this if causes issues
+            queue.add(new LogData(message, TimeUnit.MICROSECONDS.toSeconds(log.getTimeMillis())));
+        }
     }
-  }
 }
