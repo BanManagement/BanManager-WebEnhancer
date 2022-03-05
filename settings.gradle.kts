5550c153c17bc82d18cafd0a74f7efc07800a20f
James Mortemore <jamesmortemore@gmail.com>
2022-03-05 20:40:34 +0000

    feat: added bungeecord support

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,14 +2,14 @@ rootProject.name = "BanManagerWebEnhancer"
 
 include(":BanManagerWebEnhancerCommon")
 include(":BanManagerWebEnhancerBukkit")
-// include(":BanManagerWebEnhancerBungee")
+include(":BanManagerWebEnhancerBungee")
 include(":BanManagerWebEnhancerSponge")
 include(":BanManagerWebEnhancerLibs")
 //include("BanManagerWebEnhancerVelocity")
 
 project(":BanManagerWebEnhancerCommon").projectDir = file("common")
 project(":BanManagerWebEnhancerBukkit").projectDir = file("bukkit")
-// project(":BanManagerWebEnhancerBungee").projectDir = file("bungee")
+project(":BanManagerWebEnhancerBungee").projectDir = file("bungee")
 project(":BanManagerWebEnhancerSponge").projectDir = file("sponge")
 project(":BanManagerWebEnhancerLibs").projectDir = file("libs")
 //project("BanManagerVelocity").projectDir = file("velocity")
